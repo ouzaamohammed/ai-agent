8ff323373c551b17cbe61dd7ad6f9a4a2311b04e
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-06-28 02:31:43 +0100

    track history, so model can see entire conversation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,10 +11,15 @@ if len(sys.argv) != 2:
   print("Usage: python3 main.py <content>")
   sys.exit(1)
 
+user_prompt = sys.argv[1]
+messages = [
+  types.Content(role="user", parts=[types.Part(text=user_prompt)])
+]
+
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
   model="gemini-2.0-flash-001", 
-  contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+  contents=messages
 )
 
 print(response.text)
