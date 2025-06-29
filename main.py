83fd1a4ae4a1be7de8bd80ea56ce8b96c1b5c1b7
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-06-29 03:47:52 +0100

    add calculator cli to test the agent on

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 if len(sys.argv) < 2:
-  print("Usage: python3 main.py <content>")
-  sys.exit(1)
+	print("Usage: python3 main.py <content>")
+	sys.exit(1)
 
 user_prompt = sys.argv[1]
 messages = [
-  types.Content(role="user", parts=[types.Part(text=user_prompt)])
+  	types.Content(role="user", parts=[types.Part(text=user_prompt)])
 ]
 
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
-  model="gemini-2.0-flash-001", 
-  contents=messages
+  	model="gemini-2.0-flash-001", 
+  	contents=messages
 )
 
 print(response.text)
 
 if sys.argv[-1] == "--verbose":
-  print("User prompt:", user_prompt)
-  print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-  print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("User prompt:", user_prompt)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
