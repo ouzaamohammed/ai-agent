bace0edf76614e574f8b62d74a9cb31a7798b9f1
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-07-02 13:28:02 +0100

    add a dumb system prompt

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ if len(sys.argv) < 2:
 	sys.exit(1)
 
 user_prompt = sys.argv[1]
+system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
 messages = [
   	types.Content(role="user", parts=[types.Part(text=user_prompt)])
 ]
@@ -19,7 +20,8 @@ messages = [
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
   	model="gemini-2.0-flash-001", 
-  	contents=messages
+  	contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
 )
 
 print(response.text)
